dd5e6a88763f433d2e3b559f2351869790cb10e9
Ankur Anant <ankuranant99@gmail.com>
2018-09-16 00:22:13 +0530

    removing some stuff

--- botbot.js
+++ botbot.js
@@ -1,13 +1,9 @@
 module.exports = (req, res, next) => {
 	var userName = req.body.user_name;
-	// var triggerWord = req.body.trigger_word
+	
 	var botPayload = {
 		text: 'Hello ' + userName + ', welcome to the channel.'
 	}
-
-	// var hiPayload = {
-	// 	text: 'hi ' + userName + ', what\'s up'
-	// }
 
 	if(userName !== 'slackbot'){
 		return res.status(200).json(botPayload);		
