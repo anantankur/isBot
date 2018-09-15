0904d8a8f9c1d1e9ba8d626c96875cf95d1cd1fb
Ankur Anant <ankuranant99@gmail.com>
2018-09-15 23:45:36 +0530

    learning node

--- botbot.js
+++ botbot.js
@@ -1,11 +1,16 @@
 module.exports = (req, res, next) => {
 	var userName = req.body.user_name;
+	// var triggerWord = req.body.trigger_word
 	var botPayload = {
 		text: 'Hello ' + userName + ', welcome to the channel.'
 	}
 
+	// var hiPayload = {
+	// 	text: 'hi ' + userName + ', what\'s up'
+	// }
+
 	if(userName !== 'slackbot'){
-		return res.status(200).json(botPayload);
+		return res.status(200).json(botPayload);		
 	} else{
 		return res.status(200).end();
 	}
